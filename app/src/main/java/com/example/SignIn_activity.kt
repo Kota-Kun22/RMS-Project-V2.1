4dce05e9ca56ecf4595038deea7be07be69d1bd8
Harsh Rastogi <harshrastogi013@gmail.com>
2024-03-18 21:02:05 +0530

    sigIn and signOut functionality

---
+++ app/src/main/java/com/example/SignIn_activity.kt
@@ -0,0 +1,56 @@
+package com.example
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.example.rms_project_v2.R
+import com.example.rms_project_v2.databinding.ActivitySignInBinding
+import com.google.firebase.auth.FirebaseAuth
+
+class SignIn_activity : AppCompatActivity() {
+    private lateinit var binding: ActivitySignInBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding= ActivitySignInBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        firebaseAuth=FirebaseAuth.getInstance()
+         binding.signInPageSignUpButton.setOnClickListener {
+             val intent= Intent(this,signUp_activity::class.java)
+             startActivity(intent)
+         }
+
+        binding.LogINButton.setOnClickListener {
+            val email = binding.signInMail.text.toString()
+            val pass = binding.TypePasswordSignIN.text.toString()
+
+            if (email.isNotEmpty() && pass.isNotEmpty()) {
+
+                firebaseAuth.signInWithEmailAndPassword(email, pass).addOnCompleteListener {
+                    if (it.isSuccessful) {
+                        val intent = Intent(this, MainActivity::class.java)
+                        startActivity(intent)
+                    } else {
+                        Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+
+                    }
+                }
+            } else {
+                Toast.makeText(this, "Empty Fields Are not Allowed !!", Toast.LENGTH_SHORT).show()
+
+            }
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        if(firebaseAuth.currentUser != null){
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            }
+    }
+}
