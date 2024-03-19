f1f847ca0855130390d7383b5a86d927b926cc2e
rishabh3349 <116977210+rishabh3349@users.noreply.github.com>
2024-03-19 18:32:19 +0530

    add user in realtime database

--- app/src/main/java/com/example/MainActivity.kt
+++ app/src/main/java/com/example/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.example
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 import com.example.rms_project_v2.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val addCustomer: ImageView =findViewById(R.id.add_customer)
+        addCustomer.setOnClickListener{
+            startActivity(Intent(this,AddCustomerPopUp::class.java))
+        }
     }
 }
