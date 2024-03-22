6a8f0bd979ccec303cbed1f45447300e9389c1aa
rishabh3349 <116977210+rishabh3349@users.noreply.github.com>
2024-03-22 10:30:09 +0530

    home fragment crash

--- app/src/main/java/com/example/NewUser.kt
+++ app/src/main/java/com/example/NewUser.kt
@@ -1,10 +1,21 @@
 package com.example
 
-data class NewUser(
-    val uid:String,
-    val name:String,
-    val dob:String,
-    val phone_no:String,
-    val telecom:String,
-    val email:String
-)
+
+class NewUser {
+    var uid:String?=null
+    var name:String?=null
+    var dob:String?=null
+    var phone_no:String?=null
+    var telecom:String?=null
+    var email:String?=null
+    constructor(){}
+    constructor(uid:String?,name:String?,dob:String?,phone_no:String?,telecom:String?,email:String?){
+        this.uid=uid
+        this.name=name
+        this.dob=dob
+        this.phone_no=phone_no
+        this.telecom=telecom
+        this.email=email
+
+    }
+}
