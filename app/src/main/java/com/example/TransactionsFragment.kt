b690b733c160bf06d05d2c735ee76ad6fb61bc51
rishabh3349 <116977210+rishabh3349@users.noreply.github.com>
2024-04-07 20:06:54 +0530

    calender in transaction done

--- app/src/main/java/com/example/TransactionsFragment.kt
+++ app/src/main/java/com/example/TransactionsFragment.kt
@@ -1,11 +1,13 @@
 package com.example
 
+import android.app.DatePickerDialog
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -16,6 +18,7 @@ import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
+import java.util.Calendar
 
 
 class TransactionsFragment : Fragment() {
@@ -42,6 +45,8 @@ class TransactionsFragment : Fragment() {
         userRecyclerView = rootView.findViewById(R.id.transaction_rv)
         userRecyclerView.layoutManager = LinearLayoutManager(requireContext())
         userRecyclerView.adapter = adapter
+
+        val date_set:ImageView=rootView.findViewById(R.id.date_set)
 
 
         mDbRef.addListenerForSingleValueEvent(object : ValueEventListener {
@@ -73,6 +78,60 @@ class TransactionsFragment : Fragment() {
                 Log.e("FirebaseError", "Database operation cancelled: $error")
             }
         })
+
+        date_set.setOnClickListener {
+            var selectedDate:String=""
+            val calendar = Calendar.getInstance()
+            val year = calendar.get(Calendar.YEAR)
+            val month = calendar.get(Calendar.MONTH)
+            val dayOfMonth = calendar.get(Calendar.DAY_OF_MONTH)
+            val datePickerDialog = DatePickerDialog(
+                requireContext(),
+                { _, selectedYear, selectedMonth, selectedDay ->
+                    selectedDate = "$selectedDay/${selectedMonth + 1}/$selectedYear"
+                    mDbRef.addListenerForSingleValueEvent(object : ValueEventListener {
+                        override fun onDataChange(snapshot: DataSnapshot) {
+                            transactionList.clear()
+                            credit = 0
+                            paid = 0
+
+                            for (childSnapshot in snapshot.children) {
+                                val rechargeDetails = childSnapshot.getValue(RechargeDetails::class.java)
+                                rechargeDetails?.let {
+                                    if(it.date==selectedDate){
+                                        transactionList.add(it)
+                                        if (it.payment_status == "Paid") {
+                                            paid += (it.amount!!.toInt())
+                                        } else {
+                                            credit += (it.amount!!.toInt())
+                                        }
+                                    }
+                                }
+                            }
+                            adapter.notifyDataSetChanged()
+                            val creditText: TextView = rootView.findViewById(R.id.credit)
+                            val paidText: TextView = rootView.findViewById(R.id.paid)
+                            val totalText: TextView = rootView.findViewById(R.id.total)
+                            creditText.text = credit.toString()
+                            paidText.text = paid.toString()
+                            totalText.text = "₹ "+(paid + credit).toString()
+                        }
+                        override fun onCancelled(error: DatabaseError) {
+                            Log.e("FirebaseError", "Database operation cancelled: $error")
+                        }
+                    })
+                },
+                year,
+                month,
+                dayOfMonth
+            )
+            datePickerDialog.show()
+        }
+
+
+
+
+
         return rootView
     }
 }
